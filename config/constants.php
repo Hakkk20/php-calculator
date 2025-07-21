ef3976bf267a38985504a645d9c286caef665fd8
Hakkk20 <hakobtumasyan917@gmail.com>
2025-07-21 12:03:26 +0400

    Create constants.php

---
+++ config/constants.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!defined('APPROOT')) {
+    define('APPROOT', dirname(__DIR__));
+}
+
+if (!defined('URLROOT')) {
+    define('URLROOT', 'http://localhost:8000');
+}
+
+if (!defined('APPNAME')) {
+    define('APPNAME', 'Калькулятор');
+}
+
+if (!defined('DECIMAL_PRECISION')) {
+    define('DECIMAL_PRECISION', 2);
+}
+
+if (!defined('DEBUG_MODE')) {
+    define('DEBUG_MODE', true);
+}
